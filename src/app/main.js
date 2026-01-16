0f0b41f0b9c493742454cee543d59af03674311f
Mateus-Cimini <mateusmateus122333@gmail.com>
2026-01-16 04:15:59 -0300

    feat: adicionando a estrutura do 'footer'

--- src/app/main.js
+++ src/app/main.js
@@ -1,5 +1,28 @@
 import '../styles/style.css'
-
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+
+// mover para um toast.js
+import { Toast } from 'bootstrap'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const toasts = {
+    top: new Toast(document.getElementById('toastTop')),
+    jg: new Toast(document.getElementById('toastJg')),
+    mid: new Toast(document.getElementById('toastMid')),
+    adc: new Toast(document.getElementById('toastAdc')),
+    sup: new Toast(document.getElementById('toastSup'))
+  }
+
+  // expõe globalmente para o HTML se precisar
+  window.toasts = toasts
+
+  document.getElementById('liveToastTopBtn').addEventListener('click', () => toasts.top.show())
+  document.getElementById('liveToastJgBtn').addEventListener('click', () => toasts.jg.show())
+  document.getElementById('liveToastMidBtn').addEventListener('click', () => toasts.mid.show())
+  document.getElementById('liveToastAdcBtn').addEventListener('click', () => toasts.adc.show())
+  document.getElementById('liveToastSupBtn').addEventListener('click', () => toasts.sup.show())
+})
+
+// ate aqui vai o toast.js
