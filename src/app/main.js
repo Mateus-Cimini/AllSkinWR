3c1edb15456088c44812b0794fbc4d157ad78c58
Mateus-Cimini <mateusmateus122333@gmail.com>
2026-01-19 16:55:55 -0300

    style: adicionando efeito de active na navbar

--- src/app/main.js
+++ src/app/main.js
@@ -1,28 +1,48 @@
-import '../styles/style.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import "../styles/style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 // mover para um toast.js
-import { Toast } from 'bootstrap'
+import { Toast } from "bootstrap";
 
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const toasts = {
-    top: new Toast(document.getElementById('toastTop')),
-    jg: new Toast(document.getElementById('toastJg')),
-    mid: new Toast(document.getElementById('toastMid')),
-    adc: new Toast(document.getElementById('toastAdc')),
-    sup: new Toast(document.getElementById('toastSup'))
-  }
+    top: new Toast(document.getElementById("toastTop")),
+    jg: new Toast(document.getElementById("toastJg")),
+    mid: new Toast(document.getElementById("toastMid")),
+    adc: new Toast(document.getElementById("toastAdc")),
+    sup: new Toast(document.getElementById("toastSup")),
+  };
 
   // expõe globalmente para o HTML se precisar
-  window.toasts = toasts
+  window.toasts = toasts;
 
-  document.getElementById('liveToastTopBtn').addEventListener('click', () => toasts.top.show())
-  document.getElementById('liveToastJgBtn').addEventListener('click', () => toasts.jg.show())
-  document.getElementById('liveToastMidBtn').addEventListener('click', () => toasts.mid.show())
-  document.getElementById('liveToastAdcBtn').addEventListener('click', () => toasts.adc.show())
-  document.getElementById('liveToastSupBtn').addEventListener('click', () => toasts.sup.show())
-})
+  document
+    .getElementById("liveToastTopBtn")
+    .addEventListener("click", () => toasts.top.show());
+  document
+    .getElementById("liveToastJgBtn")
+    .addEventListener("click", () => toasts.jg.show());
+  document
+    .getElementById("liveToastMidBtn")
+    .addEventListener("click", () => toasts.mid.show());
+  document
+    .getElementById("liveToastAdcBtn")
+    .addEventListener("click", () => toasts.adc.show());
+  document
+    .getElementById("liveToastSupBtn")
+    .addEventListener("click", () => toasts.sup.show());
+});
 
 // ate aqui vai o toast.js
+
+// aqui é da navlink e navbar
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach((links) => {
+  links.addEventListener("click", function () {
+    navLinks.forEach((l) => l.classList.remove("active"));
+    this.classList.add('active')
+  });
+});
