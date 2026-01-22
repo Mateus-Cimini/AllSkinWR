1e890755a871f29c7b362593aeaa805682aedd7b
Mateus-Cimini <mateusmateus122333@gmail.com>
2026-01-22 20:31:24 -0300

    feat: implementando o json de personagem no select

--- src/app/main.js
+++ src/app/main.js
@@ -2,33 +2,28 @@ import "../styles/style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-//select2
+// jQuery + Select2
 import jQuery from "jquery";
 window.$ = window.jQuery = jQuery;
 
-import "select2";
 import select2 from "select2";
 select2(jQuery);
 
 import "select2/dist/css/select2.css";
 import "select2-bootstrap-5-theme/dist/select2-bootstrap-5-theme.css";
 
-import { initInputSelect } from "../shared/ui/initInputSelect.js";
-
-
-
+import { initInputSelect } from "../shared/ui/input/initInputSelect.js";
+import { loadChampionsFromJson } from "../shared/ui/input/optionInput.js";
 
 import { initToasts } from "../shared/ui/InitToast.js";
 import { initNavLinks } from "../shared/ui/initNavLinks.js";
 import { initThemeSwitcher } from "../shared/ui/initThemeSwitcher.js";
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   initToasts();
   initNavLinks();
   initThemeSwitcher();
-});
 
-jQuery(() => {
+  await loadChampionsFromJson("#champion-input");
   initInputSelect();
 });
-
